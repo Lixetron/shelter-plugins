1f4711759b98cc79a4398ad3897b25edfcc079b7
Lixetron <khlopchenko_dmitriy@mail.ru>
2024-09-15 23:12:30 +0300

    feat: add screenshareQualityFix plugin, add lock-files to .gitignore

---
+++ plugins/screenshareQualityFix/settings.jsx
@@ -0,0 +1,39 @@
+const {
+    plugin: {store},
+    ui: {TextBox, Header, HeaderTags}
+} = shelter;
+
+
+function isValidAspectRatio(str) {
+    let aspectRatioPattern = /^\d+\s*\/\s*\d+$/;
+    return aspectRatioPattern.test(str);
+}
+
+export default () => (
+    <>
+        <Header tag={HeaderTags.H1}>Resolution</Header>
+        <TextBox
+            placeholder="720/1080/1440"
+            value={Number.isSafeInteger(store.resolution) ? store.resolution : ""}
+            onInput={(v) => {
+                store.resolution = parseInt(v);
+            }}
+        />
+        <Header tag={HeaderTags.H1}>Aspect Ratio</Header>
+        <TextBox
+            placeholder="'16 / 9', '21 / 9'"
+            value={isValidAspectRatio(store.aspectRatio) ? store.aspectRatio : ""}
+            onInput={(v) => {
+                store.aspectRatio = v;
+            }}
+        />
+        <Header tag={HeaderTags.H1}>FPS</Header>
+        <TextBox
+            placeholder="15/30/60"
+            value={Number.isSafeInteger(store.fps) ? store.fps : ""}
+            onInput={(v) => {
+                store.fps = parseInt(v);
+            }}
+        />
+    </>
+);
